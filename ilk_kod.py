e77818b2171f48552a3caae1877dba303eb538a3
Semih <96semih@gmail.com>
2026-02-02 00:09:13 +0300

    input ozelligi eklendi

--- ilk_kod.py
+++ ilk_kod.py
@@ -1,4 +1,3 @@
-print("Merhaba GitHub!")
 print("Benim adim Semih ve bu kodu terminalden yazdim.")
 
 sayi1 = 10
@@ -6,3 +5,9 @@ sayi2 = 20
 toplam = sayi1 + sayi2
 
 print(f"Islem Sonucu: {toplam}")
+
+print("-" * 30)
+isim = input("Adiniz nedir? : ")
+yas = int(input("Yasinizi girin : "))
+dogum_yili = 2026 - yas
+print(f"Memnun oldum {isim}, demek {dogum_yili} dogumlusun!")
